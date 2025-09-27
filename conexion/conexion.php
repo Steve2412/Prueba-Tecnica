72a6085692606d73d01ac0d04aa118c31151541e
Steve <stefano007casa@hotmail.com>
2025-09-26 21:12:33 -0400

    V1.1: Changes Database Connection

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,9 +1,18 @@
 <?php
 //Realizar la conexion a la base de datos
 
+//Datos de conexion
+
+$host = "localhost";
+$port = "3306";
+$usuario = "root";
+$password = "";
+$base_de_datos = "lista_espera";
+
+//Intentar la conexiongi
 
 try {
-    $conectar = new PDO('mysql:host=localhost;port=3306;dbname=lista_espera', 'root', '');
+    $conectar = new PDO("mysql:host=$host;port=$port;dbname=$base_de_datos", $usuario, $password);
     $conectar->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conectar->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     $conectar->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
